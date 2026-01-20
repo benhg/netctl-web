b1a159b752d1acf8f658fc31c145b32bd7782d56
Ben Glick <glick@glick.cloud>
2026-01-19 20:29:26 -0800

    add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base: "./",
   build: {
-    outDir: "docs",
+    outDir: "dist",
   },
   plugins: [react(), tailwindcss()],
 });
