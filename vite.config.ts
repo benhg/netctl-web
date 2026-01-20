f3520c40e14b9e724f5fb17f5e46e321899eda39
Ben Glick <glick@glick.cloud>
2026-01-19 22:02:13 -0800

    edit docs

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base: "./",
   build: {
-    outDir: "dist",
+    outDir: "docs",
   },
   plugins: [react(), tailwindcss()],
 });
